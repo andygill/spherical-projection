995142f4263e399e574f3907e7035bb9f60e4cec
Andy Gill <andygill@ku.edu>
2020-05-30 11:50:19 -0500

    Split into Main and Expr

--- Main.hs
+++ Main.hs
@@ -13,109 +13,14 @@ import Debug.Trace
 import Prelude hiding (sin, cos, atan2, (^))
 import qualified Prelude as P
 
+import Expr
+
 --class Eval e where
 --  eval :: () -> e -> Number
 
 class Conditional e where
   ifZero :: Number -> e -> e -> e
 
-data Expr :: * -> * where
-  ExpNumber :: Double -> Expr t
-  ExpSin    :: t -> Expr t
-  ExpCos    :: t -> Expr t
-  ExpSqrt   :: t -> Expr t
-  ExpAdd    :: t -> t -> Expr t
-  ExpSub    :: t -> t -> Expr t
-  ExpMul    :: t -> t -> Expr t
-  ExpDiv    :: t -> t -> Expr t
-  ExpPower  :: t -> t -> Expr t
-  ExpAtan2  :: t -> t -> Expr t
-  ExpLambda :: Int -> t -> Expr t
-  ExpVar    :: Int -> Expr t
-  ExpLambda' :: t -> t -> Expr t
-  ExpVar'   :: Dynamic -> Expr t
-  ExpRectilinear :: t -> t -> Expr t
-  ExpIfZero :: t -> t -> t -> Expr t    
-
-deriving instance Show t => Show (Expr t)
-
-instance Functor Expr where
-  fmap f (ExpNumber d) = ExpNumber d
-  fmap f (ExpSin t1) = ExpSin (f t1)
-  fmap f (ExpCos t1) = ExpCos (f t1)
-  fmap f (ExpSqrt t1) = ExpSqrt (f t1)
-  fmap f (ExpAdd t1 t2) = ExpAdd (f t1) (f t2)
-  fmap f (ExpSub t1 t2) = ExpSub (f t1) (f t2)
-  fmap f (ExpMul t1 t2) = ExpMul (f t1) (f t2)
-  fmap f (ExpDiv t1 t2) = ExpDiv (f t1) (f t2)
-  fmap f (ExpPower t1 t2) = ExpPower (f t1) (f t2)
-  fmap f (ExpAtan2 t1 t2) = ExpAtan2 (f t1) (f t2)
-  fmap f g = error "fmap"
-instance Foldable Expr where
-  foldr f z (ExpNumber d) = z
-  foldr f z (ExpSin t1) = f t1 z
-  foldr f z (ExpCos t1) = f t1 z
-  foldr f z (ExpSqrt t1) = f t1 z
-  foldr f z (ExpAdd t1 t2) = f t1 (f t2 z)
-  foldr f z (ExpSub t1 t2) = f t1 (f t2 z)
-  foldr f z (ExpMul t1 t2) = f t1 (f t2 z)
-  foldr f z (ExpDiv t1 t2) = f t1 (f t2 z)
-  foldr f z (ExpPower t1 t2) = f t1 (f t2 z)
-  foldr f z (ExpAtan2 t1 t2) = f t1 (f t2 z)
-  foldr f z _ = error "foldr"
-instance Traversable Expr where
-  traverse f (ExpNumber d) = pure $ ExpNumber d
-  traverse f (ExpSin t1) = ExpSin <$> f t1
-  traverse f (ExpCos t1) = ExpCos <$> f t1
-  traverse f (ExpSqrt t1) = ExpSqrt <$> f t1
-  traverse f (ExpAdd t1 t2) = ExpAdd <$> f t1 <*> f t2
-  traverse f (ExpSub t1 t2) = ExpSub <$> f t1 <*> f t2
-  traverse f (ExpMul t1 t2) = ExpMul <$> f t1 <*> f t2
-  traverse f (ExpDiv t1 t2) = ExpDiv <$> f t1 <*> f t2
-  traverse f (ExpPower t1 t2) = ExpPower <$> f t1 <*> f t2
-  traverse f (ExpAtan2 t1 t2) = ExpAtan2 <$> f t1 <*> f t2
-  traverse f (ExpRectilinear t1 t2) = ExpRectilinear <$> f t1 <*> f t2
-  traverse f (ExpIfZero t1 t2 t3) = ExpIfZero <$> f t1 <*> f t2 <*> f t3
-  traverse f (ExpVar i) = pure $ ExpVar i
-  traverse _ _ = error "traverse"
-
-newtype Mu a = Mu (a (Mu a))
-
-instance Show (Mu Expr) where
-  showsPrec d (Mu (ExpNumber n)) = shows n
-  showsPrec d (Mu (ExpSin t)) = showParen (d > 10) $
-      showString "sin " . showsPrec 11 t
-  showsPrec d (Mu (ExpCos t)) = showParen (d > 10) $
-      showString "cos " . showsPrec 11 t
-  showsPrec d (Mu (ExpSqrt t)) = showParen (d > 10) $
-      showString "sqrt " . showsPrec 11 t
-  showsPrec d (Mu (ExpAdd n1 n2)) = showParen (d > 6) $
-      showsPrec 7 n1  .
-      showString " + " .
-      showsPrec 7 n2 
-  showsPrec d (Mu (ExpSub n1 n2)) = showParen (d > 6) $
-      showsPrec 7 n1  .
-      showString " - " .
-      showsPrec 7 n2 
-  showsPrec d (Mu (ExpMul n1 n2)) = showParen (d > 7) $
-      showsPrec 8 n1  .
-      showString " * " .
-      showsPrec 8 n2 
-  showsPrec d (Mu (ExpDiv n1 n2)) = showParen (d > 7) $
-      showsPrec 8 n1  .
-      showString " / " .
-      showsPrec 8 n2 
-  showsPrec d (Mu (ExpPower n1 n2)) = showParen (d > 8) $
-      showsPrec 9 n1  .
-      showString " ^ " .
-      showsPrec 9 n2
-  showsPrec d (Mu (ExpIfZero a b c)) = showParen (d > 10) $
-      showString "ifZero " .
-      showsPrec 11 a .
-      showString " " .
-      showsPrec 11 b .
-      showString " " .
-      showsPrec 11 c
 
 class Eval e where
   eval :: Expr e -> e
@@ -347,48 +252,9 @@ instance MuRef Scalar where
   type DeRef Scalar = Expr
   mapDeRef f (Scalar s) = mapDeRef f s
 
-instance MuRef (Mu Expr) where
-  type DeRef (Mu Expr) = Expr
-  mapDeRef f (Mu e) = traverse f e 
-
-class Var a where
-  mkVar :: Int -> a
-  mkVar' :: Typeable b => (a -> b) -> a
-
-class Body a where
-  maxVar :: a -> Int
-
 instance Var Scalar where
   mkVar = Scalar . Mu . ExpVar
-  mkVar' = Scalar . Mu . ExpVar' . toDyn
-
-instance (Var a, Body b, MuRef b, DeRef b ~ Expr)  => MuRef (a -> b) where
-  type DeRef (a -> b) = Expr
-  mapDeRef f fn = ExpLambda n <$> f r
-    where
-      (n,r) = maxApp fn
 
 instance Body Scalar where
   maxVar (Scalar e) = maxVar e
-
-maxApp :: (Body b, Var t) => (t -> b) -> (Int, b)
-maxApp fn = traceShow ("maxapp",n) (n,r)
-    where
-      n = maxVar r + 1
-      r = fn (mkVar n)
-
-instance (Var a, Body b) => Body (a -> b) where
-  maxVar = fst . maxApp 
-
-instance Body (Mu Expr) where
-  maxVar (Mu e) = case e of
-    ExpVar i      -> 0
-    ExpLambda i e -> i  -- This short-cut is vital to avoid 
-    other -> foldr (+) 0 $ fmap maxVar other
-
-{-
-instance MuRef b => MuRef (a -> b) where
-  type DeRef (a -> b) = Expr
-  mapDeRef f fn = f (fn (error "arg"))
--}
 
