15ece0fbe52727b7b88162f207f11ec4b6f14558
ToeFluf <jakday777@gmail.com>
2020-08-10 13:21:50 -0500

    Add JS and Haskell Expr forms

--- Expr.hs
+++ Expr.hs
@@ -361,18 +361,73 @@ num_pi :: Double
 num_pi = 3.141592653589793
 ------------------------------------------------------------------------------
 
+listToTuple :: Show a => [a] -> String
+listToTuple [] = ""
+listToTuple (x:[]) = show x
+listToTuple (x:xs) = (show x) ++ ", " ++ (listToTuple xs)
+
 newtype JavaScript = JavaScript ExprFunction
 
 instance Show JavaScript where
   show (JavaScript (ExprFunction as xs r)) = unlines $
-      ["((" ++ show as ++ ") => {"] ++
+      ["((" ++ (if null as then "" else show as) ++ ") => {"] ++
       map showAssign xs ++
       [ "  return " ++ show r ++ ";"
       , "})"
       ]
     where
-      showAssign (v,ExpScalar e) = "  let " ++ show v ++ " = " ++ show e ++ ";"
-      showAssign (v,ExpSin e) = "  let " ++ show v ++ " = Math.sin(" ++ show e ++ ");"
-      showAssign (v,ExpIfZero i t e) = "  let " ++ show v ++
-                 " = " ++ show i ++ "?" ++ show t ++ ":" ++ show e ++ ";"
+      showAssign (v,ExpScalar e)    = "  let " ++ show v ++ " = " ++ show e ++ ";"
+      showAssign (v,ExpTuple es)    = "  let " ++ show v ++ " = " ++ show es ++ ";"
+      showAssign (v,ExpSin e)       = "  let " ++ show v ++ " = Math.sin(" ++ show e ++ ");"
+      showAssign (v,ExpCos e)       = "  let " ++ show v ++ " = Math.cos(" ++ show e ++ ");"
+      showAssign (v,ExpTan e)       = "  let " ++ show v ++ " = Math.tan(" ++ show e ++ ");"
+      showAssign (v,ExpAsin e)      = "  let " ++ show v ++ " = Math.asin(" ++ show e ++ ");"
+      showAssign (v,ExpAcos e)      = "  let " ++ show v ++ " = Math.acos(" ++ show e ++ ");"
+      showAssign (v,ExpAtan e)      = "  let " ++ show v ++ " = Math.atan(" ++ show e ++ ");"
+      showAssign (v,ExpSqrt e)      = "  let " ++ show v ++ " = Math.sqrt(" ++ show e ++ ");"
+      showAssign (v,ExpAbs e)       = "  let " ++ show v ++ " = Math.abs(" ++ show e ++ ");"
+      showAssign (v,ExpSignum e)    = "  let " ++ show v ++ " = Math.sign(" ++ show e ++ ");"
+      showAssign (v,ExpNeg e)       = "  let " ++ show v ++ " = (-1) * " ++ show e ++ ";"
+      showAssign (v,ExpAdd e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " + " ++ show e2 ++ ";"
+      showAssign (v,ExpSub e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " - " ++ show e2 ++ ";"
+      showAssign (v,ExpMul e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " * " ++ show e2 ++ ";"
+      showAssign (v,ExpDiv e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " / " ++ show e2 ++ ";"
+      showAssign (v,ExpPower e1 e2) = "  let " ++ show v ++ " = Math.pow(" ++ show e1 ++ ", " ++ show e2 ++");"
+      showAssign (v,ExpAtan2 e1 e2) = "  let " ++ show v ++ " = Math.atan2(" ++ show e1 ++ ", " ++ show e2 ++");"
+      showAssign (v,ExpRectilinear e1 e2) = "  let " ++ show v ++ " = { x : " ++ show e1 ++ ", y : " ++ show e2 ++"};"
+      showAssign (v,ExpFisheye e1 e2) = "  let " ++ show v ++ " = { r : " ++ show e1 ++ ", t : " ++ show e2 ++"};"
+      showAssign (v,ExpIfZero i t e) = "  let " ++ show v ++ " = " ++ show i ++ "?" ++ show t ++ ":" ++ show e ++ ";"
+      --showAssign (v,ExpVar i)
       showAssign (v,e) = "  // let " ++ show v ++ " = " ++ show e ++ ";"
+
+newtype Haskell = Haskell ExprFunction
+
+instance Show Haskell where
+  show (Haskell (ExprFunction as xs r)) = unlines $
+      ["(\\(" ++ (listToTuple as) ++ ") -> do "] ++
+      map showAssign xs ++
+      [ show r,")"]
+    where
+      showAssign (v,ExpScalar e)    = "  let " ++ show v ++ " = " ++ show e
+      showAssign (v,ExpTuple es)    = "  let " ++ show v ++ " = (" ++ listToTuple es ++ ")"
+      showAssign (v,ExpSin e)       = "  let " ++ show v ++ " = sin " ++ show e
+      showAssign (v,ExpCos e)       = "  let " ++ show v ++ " = cos " ++ show e
+      showAssign (v,ExpTan e)       = "  let " ++ show v ++ " = tan " ++ show e
+      showAssign (v,ExpAsin e)      = "  let " ++ show v ++ " = asin " ++ show e
+      showAssign (v,ExpAcos e)      = "  let " ++ show v ++ " = acos " ++ show e
+      showAssign (v,ExpAtan e)      = "  let " ++ show v ++ " = atan " ++ show e
+      showAssign (v,ExpSqrt e)      = "  let " ++ show v ++ " = sqrt " ++ show e
+      showAssign (v,ExpAbs e)       = "  let " ++ show v ++ " = abs " ++ show e
+      showAssign (v,ExpSignum e)    = "  let " ++ show v ++ " = signum " ++ show e
+      showAssign (v,ExpNeg e)       = "  let " ++ show v ++ " = neg " ++ show e
+      showAssign (v,ExpAdd e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " + " ++ show e2
+      showAssign (v,ExpSub e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " - " ++ show e2
+      showAssign (v,ExpMul e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " * " ++ show e2
+      showAssign (v,ExpDiv e1 e2)   = "  let " ++ show v ++ " = " ++ show e1 ++ " / " ++ show e2
+      showAssign (v,ExpPower e1 e2) = "  let " ++ show v ++ " = " ++ show e1 ++ " ^ " ++ show e2
+      showAssign (v,ExpAtan2 e1 e2) = "  let " ++ show v ++ " = atan2 " ++ show e1 ++ " " ++ show e2
+      showAssign (v,ExpRectilinear e1 e2) = "  let " ++ show v ++ " = (" ++ show e1 ++ ", " ++ show e2 ++ ")"
+      showAssign (v,ExpFisheye e1 e2)= "  let " ++ show v ++ " = (" ++ show e1 ++ ", " ++ show e2 ++ ")"
+      showAssign (v,ExpIfZero i t e) ="  let " ++ show v ++ " = if " ++ show i ++ " then " ++ show t ++ " else " ++ show e
+      --showAssign (v,ExpVar i)
+      showAssign (v,e) = "  // let " ++ show v ++ " = " ++ show e
